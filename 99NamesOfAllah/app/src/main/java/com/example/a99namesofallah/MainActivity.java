8a12d761786acb9bf31eed98d848bed96e211bc6
farzana-moury <w0737626@myscc.ca>
2022-03-20 00:28:24 -0400

    #3 #4 Changes to layouts
    Changed the layout of the app bar, as well as the respective pages.

--- 99NamesOfAllah/app/src/main/java/com/example/a99namesofallah/MainActivity.java
+++ 99NamesOfAllah/app/src/main/java/com/example/a99namesofallah/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.a99namesofallah;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -16,7 +12,6 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.a99namesofallah.databinding.ActivityMainBinding;
 
 import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,21 +37,6 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
