aaaf31143ab86fb460f2b166d5730c3350cf65db
farzana-moury <w0737626@myscc.ca>
2022-03-21 22:38:15 -0400

    #5 #3 Complete Name Set Class
    class represents what a single name set is supposed to contain:
    - arabic name
    - english name
    - meaning

--- 99NamesOfAllah/app/src/main/java/com/example/a99namesofallah/Lists/Lists.java
+++ 99NamesOfAllah/app/src/main/java/com/example/a99namesofallah/Lists/Lists.java
@@ -1,25 +1,9 @@
 package com.example.a99namesofallah.Lists;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import com.example.a99namesofallah.R;
-import com.example.a99namesofallah.R.string;
-
-import android.content.res.Resources;
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-public class Lists extends Fragment{
+public class Lists{
     //lists for all 99 names, including arabic and english, as well as their meanings
     public static List<String> arabicNames = new ArrayList<>();
     public static List<String> englishNames = new ArrayList<>();
---
+++ 99NamesOfAllah/app/src/main/java/com/example/a99namesofallah/NameSet/NameSet.java
@@ -0,0 +1,74 @@
+package com.example.a99namesofallah.NameSet;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.a99namesofallah.R;
+
+public class
+NameSet extends Fragment {
+
+    // constants to represent each piece of information on a particular rating
+    private static final String ARABIC_NAME = "arabic_name";
+    private static final String ENGLISH_NAME = "english";
+    private static final String MEANING = "meaning";
+
+    // properties that will hold the information
+    private String arabicName;
+    private String englishName;
+    private String meaning;
+
+    public static NameSet newInstance(String arabicName, String englishName, String meaning) {
+        NameSet fragment = new NameSet();
+        // putting data into the bundle to be transferred to another fragment
+        Bundle args = new Bundle();
+        args.putString(ARABIC_NAME, arabicName);
+        args.putString(ENGLISH_NAME, englishName);
+        args.putString(MEANING, meaning);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // taking the values from the bundle
+        if (getArguments() != null) {
+            arabicName = getArguments().getString(ARABIC_NAME);
+            englishName = getArguments().getString(ENGLISH_NAME);
+            meaning = getArguments().getString(MEANING);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.name_set, container, false);
+
+        // if there is a value within each information,
+        if( arabicName != null) {
+            TextView arabName = view.findViewById(R.id.arab_name); // arabic name
+            arabName.setText(arabicName);
+        }
+
+        if( englishName != null) {
+            TextView arabName = view.findViewById(R.id.eng_name); // english name
+            arabName.setText(arabicName);
+        }
+
+        if( meaning != null) {
+            TextView meaningText = view.findViewById(R.id.meaning); // meaning of the name
+            meaningText.setText(meaning);
+        }
+
+        return view;
+    }
+}
